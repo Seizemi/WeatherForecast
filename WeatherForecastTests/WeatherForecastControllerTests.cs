2bae3b75b3646cd57ba735ef3363ef4122602932
MAXIME DELOBELLE <maxdelob@gmail.com>
2026-04-13 09:22:37 -0400

    Creation of tests project

---
+++ WeatherForecastTests/WeatherForecastControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Controllers;
+
+namespace WeatherForecastTests
+{
+    [TestClass]
+    public sealed class WeatherForecastControllerTests
+    {
+        IFixture? _fixture;
+        ILogger<WeatherForecastController>? _loggerMock;
+        WeatherForecastController? _sut;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _loggerMock = _fixture.Freeze<ILogger<WeatherForecastController>>();
+            _sut = new(_loggerMock);
+        }
+
+        [TestMethod]
+        public void WeatherForecastController_Get_ShouldReturnNonEmptyWeatherForecast()
+        {
+            IEnumerable<WebApplication1.WeatherForecast>? result = _sut?.Get();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
